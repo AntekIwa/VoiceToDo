041fb0c51e01df96a9eaaf581794581c1f429798
Antoni_Iwanowski <75222159+AntekIwa@users.noreply.github.com>
2025-07-05 22:52:21 +0200

    Create todo.py

---
+++ todo.py
@@ -0,0 +1,28 @@
+class TODO:
+    def __init__(self):
+        self.todo = []
+    def add(self, task):
+        if task in self.todo:
+            return False
+        self.todo.append(task)
+    def del_task(self, task):
+        if task not in self.todo:
+            return False
+        self.todo.remove(task)
+    def del_idx(self, idx):
+        if idx > len(self.todo):
+            return False
+        self.todo.pop(idx - 1)
+    def printt(self):
+        print("Rozmiar listy: ", len(self.todo))
+        for i in range(len(self.todo)):
+            print(i + 1, self.todo[i])
+        
+lista = TODO()
+lista.add("matma")
+lista.add("Polski")
+lista.printt()
+lista.del_task("matma")
+lista.printt()
+lista.del_idx(1)
+lista.printt()
