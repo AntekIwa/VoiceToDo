4c643a1425bf89133968a565f9c35466d6c42b1d
Antoni_Iwanowski <75222159+AntekIwa@users.noreply.github.com>
2025-07-06 10:11:47 +0200

    Update todo.py

--- todo.py
+++ todo.py
@@ -5,14 +5,17 @@ class TODO:
         if task in self.todo:
             return False
         self.todo.append(task)
+        return True
     def del_task(self, task):
         if task not in self.todo:
             return False
         self.todo.remove(task)
+        return True
     def del_idx(self, idx):
         if idx > len(self.todo):
             return False
         self.todo.pop(idx - 1)
+        return True
     def printt(self):
         print("Rozmiar listy: ", len(self.todo))
         for i in range(len(self.todo)):
