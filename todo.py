9a562e6f9d482086b906a68853ba507d49c9239f
Antoni_Iwanowski <75222159+AntekIwa@users.noreply.github.com>
2025-07-09 14:21:49 +0200

    Update todo.py

--- todo.py
+++ todo.py
@@ -1,31 +1,51 @@
+import os
+import json
+
 class TODO:
-    def __init__(self):
-        self.todo = []
+    def __init__(self, filename = "todo_data.json"):
+        self.filename = filename
+        self.todo = self.load()
+
+    def load(self):
+        if not os.path.exists(self.filename):
+            return []
+        with open(self.filename, "r", encoding="utf-8") as f:
+            return json.load(f)
+
+    def save(self):
+        with open(self.filename, "w", encoding="utf-8") as f:
+            json.dump(self.todo, f, ensure_ascii=False, indent=2)
+
     def add(self, task):
         if task in self.todo:
             return False
         self.todo.append(task)
+        self.save()
         return True
+
     def del_task(self, task):
         if task not in self.todo:
             return False
         self.todo.remove(task)
+        self.save()
         return True
+
     def del_idx(self, idx):
         if idx > len(self.todo):
             return False
         self.todo.pop(idx - 1)
+        self.save()
         return True
+
     def printt(self):
         print("Rozmiar listy: ", len(self.todo))
         for i in range(len(self.todo)):
             print(i + 1, self.todo[i])
-        
+
+
 lista = TODO()
 lista.add("matma")
 lista.add("Polski")
-lista.printt()
-lista.del_task("matma")
-lista.printt()
-lista.del_idx(1)
-lista.printt()
+text = ""
+for i in range(len(lista.todo)):
+    text += str(i + 1) + " "  + lista.todo[i] + " "
